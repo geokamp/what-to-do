b12f3224c3db77b734cdb340b6ef75e4587247d7
geokamp <geokamp98@gmail.com>
2025-04-08 11:54:03 +0300

    fetchall

--- app/addTopic/page.js
+++ app/addTopic/page.js
@@ -27,7 +27,7 @@ export default function AddTopic(){
         }
 
         try{
-            const res = await fetch("http://localhost:3000/api/topics", {
+            const res = await fetch("/api/topics", {
                 method: "POST",
                 headers:{
                     "Content-type": "application/json",
